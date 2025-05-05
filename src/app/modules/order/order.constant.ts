395b19b240fe3f2a12722a350292b3b397123a7c
MarGoN <monondcosta@gmail.com>
2025-05-05 06:04:19 +0600

    Modify: modified order constant

--- src/app/modules/order/order.constant.ts
+++ src/app/modules/order/order.constant.ts
@@ -1,2 +1,2 @@
 export const shippingStatus = ['PENDING', 'PROCESSING', 'SHIPPED', 'DELIVERED', 'CANCELED'] as const
-export const paymentStatus = ['UNPAID', 'PAID'] as const
+export const paymentStatus = ['UNPAID', 'PAID', 'REFUNDED'] as const
